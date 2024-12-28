98d03352d9a58d77efe5fe915f275e053e0ff591
harmainbashir15 <harmainfatima127@gmail.com>
2024-12-29 01:21:52 +0500

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import ReduxProvider from "./provider";
+
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "HB CLOTHING | ",
+  description: "Women clothes store ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ReduxProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+import WinterCollection from "@/components/winter-collection";
+import Featured from "@/components/top-selling"
+
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <WinterCollection />
+      <Featured />
+    </main>
+  );
+}
